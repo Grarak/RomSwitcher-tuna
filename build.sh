84d12191bb71ab394ec78584548b7ad752a46686
Grarak <williye97@gmail.com>
2013-10-27 00:20:47 +0200

    remove cmdlines
    Signed-off-by: Grarak <williye97@gmail.com>

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ build () {
     cd boot.img-ramdisk
     find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
     cd ..
-    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz --cmdline "console=ttyFIQ0 androidboot.console=ttyFIQ0 mem=1G vmalloc=768M omap_wdt.timer_margin=30 no_console_suspend" -o boot.img
+    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz -o boot.img
 }
 
 if [ -e ~/.bash_profile ]; then
