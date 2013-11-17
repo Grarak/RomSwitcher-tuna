c91aa5bcd808ff32b5b3eeecbcb8be81dfb67961
Grarak <williye97@gmail.com>
2013-11-17 19:03:02 +0100

    set values in gk.sh
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/gk.sh
+++ boot.img-ramdisk/sbin/ext/gk.sh
@@ -11,6 +11,13 @@ echo 0 > /proc/sys/kernel/randomize_va_space
 
 echo 180000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 350000 > /sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq
+
+echo 512 > /sys/block/mmcblk0/queue/read_ahead_kb
+
+echo 0 > /sys/kernel/debug/smartreflex/sr_core/autocomp
+echo 0 > /sys/kernel/debug/smartreflex/sr_iva/autocomp
+echo 0 > /sys/kernel/debug/smartreflex/sr_mpu/autocomp
 
 echo "-10 0 -10" > /sys/class/misc/colorcontrol/v1_offset
 echo "1800000000 1800000000 2000000000" > /sys/class/misc/colorcontrol/multiplier
