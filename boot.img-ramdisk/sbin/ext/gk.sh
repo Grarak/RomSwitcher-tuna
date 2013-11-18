96ee001b57d396aa8d33fccb45a91fc8dd05e802
Grarak <williye97@gmail.com>
2013-11-18 18:06:04 +0100

    enable smartreflex
    Signed-off-by: Grarak <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/gk.sh
+++ boot.img-ramdisk/sbin/ext/gk.sh
@@ -13,9 +13,6 @@ echo 180000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 350000 > /sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq
 
-echo 0 > /sys/kernel/debug/smartreflex/sr_core/autocomp
-echo 0 > /sys/kernel/debug/smartreflex/sr_iva/autocomp
-
 echo "-10 0 -10" > /sys/class/misc/colorcontrol/v1_offset
 echo "1800000000 1800000000 2000000000" > /sys/class/misc/colorcontrol/multiplier
 echo 0 > /sys/devices/virtual/misc/fsynccontrol/fsync_enabled
